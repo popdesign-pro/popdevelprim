b5f6ab8d92f8c2b22c94a42e25e773e24f245c6f
popdevel <pop.design@icloud.com>
2022-12-01 21:11:08 +0200

    lesson 10 elzero

--- js/skills.js
+++ js/skills.js
@@ -25,3 +25,46 @@
 
 // let print = (num1, num2) => num1 + num2;
 // console.log(print(1, 3));
+// let names = function (...fName) {
+//   return `String ${fName.join(", ")} => Done !`;
+// };
+// let names = (...fName) => `String ${fName.join(", ")} => Done !`;
+// document.write(names("Peter", "Beren", "Dany"));
+
+// window.onload = function () {
+//   document.getElementById("po").style.color = "red";
+// };
+
+// document.querySelector("h1").style.color = "orange";
+/*
+document.querySelector("h1").style.color = "orange";
+document.querySelector("h1").style.fontSize = "80px";
+document.querySelector("h1").style.fontWeight = "bold";
+document.querySelector("h1").style.fontFamily = "Arial";
+console.log("%cpop %cdev %cel", "color:red;font-size:30px", "background-color:blue;font-size:30px", "color:orange;font-size:30px");
+let arry=["Peter",'beren', 'dany']
+console.table(arry)
+*/
+/*  lesson from 10 to 17 =========>>
+*/
+let numberOne,numberTwo;
+numberOne = 10;
+numberTwo = 20;
+console.log(numberOne + "" + numberTwo)
+console.log(typeof (numberOne + "" + numberTwo))
+console.log((numberOne.toString() + numberTwo.toString()));
+console.log(typeof (numberOne.toString() + numberTwo.toString()));
+console.log(`${numberOne}${numberTwo}`);
+console.log(typeof `${numberOne}${numberTwo}`);
+console.log(numberTwo + "\n" + numberOne);
+console.log(`${numberTwo}
+${numberOne}`)
+let a = 21;
+let b = 20;
+console.log("_" + (a + "_" + b).repeat(4) + "_"); // _21_2021_2021_2021_20_
+/*  lesson from 10 to 17 =========>>
+
+
+*/
+
+
